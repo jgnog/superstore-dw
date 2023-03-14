342442890f7fb9c37a44a363cde65f0d399503c4
Gonçalo Nogueira <jgoncalonogueira@gmail.com>
2023-03-14 14:20:40 +0000

    Fill valid_from and valid_to dates with far away dates

--- superstore_dw/models/dim_customer.sql
+++ superstore_dw/models/dim_customer.sql
@@ -1,9 +1,24 @@
+with first_version as (
+    select
+        dbt_scd_id,
+        customer_id,
+        customer_name,
+        segment,
+        dbt_valid_from,
+        dbt_valid_to,
+        dbt_updated_at,
+        row_number() over(partition by customer_id order by dbt_valid_from) as row_nr
+    from {{ ref('sp_customer') }}
+)
 select
     dbt_scd_id as sk_customer,
     customer_id,
     customer_name,
     segment,
-    dbt_valid_from as valid_from,
-    dbt_valid_to as valid_to,
+    case
+        when row_nr = 1 then '1970-01-01'
+        else dbt_valid_from
+    end as valid_from,
+    coalesce(dbt_valid_to, '2200-01-01') as valid_to,
     dbt_updated_at as last_updated_at
-from {{ ref("sp_customer") }}
+from first_version
--- superstore_dw/models/dim_geography.sql
+++ superstore_dw/models/dim_geography.sql
@@ -1,3 +1,17 @@
+with first_version as (
+    select
+        dbt_scd_id,
+        city_id,
+        city_name,
+        state,
+        country,
+        region,
+        dbt_valid_from,
+        dbt_valid_to,
+        dbt_updated_at,
+        row_number() over(partition by city_id order by dbt_valid_from) as row_nr
+    from {{ ref('sp_city') }}
+)
 select
     dbt_scd_id as sk_geography,
     city_id,
@@ -5,7 +19,10 @@ select
     state,
     country,
     region,
-    dbt_valid_from as valid_from,
-    dbt_valid_to as valid_to,
+    case
+        when row_nr = 1 then '1970-01-01'
+        else dbt_valid_from
+    end as valid_from,
+    coalesce(dbt_valid_to, '2200-01-01') as valid_to,
     dbt_updated_at as last_updated_at
-from {{ ref("sp_city") }}
+from first_version
