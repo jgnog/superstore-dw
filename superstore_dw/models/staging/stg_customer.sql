600edececb449fa2da45c225848a15fc8d2f032c
Gonçalo Nogueira <jgoncalonogueira@gmail.com>
2024-04-18 14:29:42 +0100

    Fix stg_customer

--- superstore_dw/models/staging/stg_customer.sql
+++ superstore_dw/models/staging/stg_customer.sql
@@ -1,6 +1,6 @@
 select
-    e.customer_id,
-    e.name as customer_name,
+    c.customer_id,
+    c.name as customer_name,
     s.segment
-from {{ source("norm", "t_customer") }} e
-    join {{ source("norm", "t_segment") }} s on s.segment_id = s.segment_id
+from {{ source("norm", "t_customer") }} c
+    join {{ source("norm", "t_segment") }} s on s.segment_id = c.segment_id
